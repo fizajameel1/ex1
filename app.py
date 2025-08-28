45bb27909889aeebab6c5afb155ac112d156bf4f
fizajameel1 <jameelfiza9@gmail.com>
2025-08-28 12:26:48 +0500

    intentional bug

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flaskkk import Flask, render_template
 
 app = Flask(__name__)
 
@@ -12,3 +12,4 @@ def about():
 
 if __name__ == '__main__':
     app.run(debug=True)
+    
